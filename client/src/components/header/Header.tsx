4cb57594af8bc2f42444c596f9763ddf6272fb5b
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-25 13:50:11 +0300

    create Upload image component

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="transition hover:scale-105 w-fit p-2 absolute right-2 top-2 rounded-md border border-gray-200">
-      <Link to="/upload">Upload an image</Link>
+    <div className="flex justify-between p-2 px-5">
+      <div className="transition hover:scale-105 w-36 text-center p-2 rounded-xl border-2 border-gray-400">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="transition hover:scale-105 w-36 text-center p-2 rounded-xl border-2 border-gray-400">
+        <Link to="/upload">Upload Image</Link>
+      </div>
     </div>
   );
 };
--- client/src/components/upload/Upload.tsx
+++ client/src/components/upload/Upload.tsx
@@ -1,9 +1,70 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+import { useState } from "react";
+import UploadForm from "./UploadForm";
 const Upload = () => {
-    return (
-        <div>
-                Upload
-        </div>
-    );
+  const [image, setImage] = useState<string | null>();
+  const [imageData, setImageData] = useState<any>(null);
+
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!imageData || !title || !description)
+      return alert("Please enter all fields");
+
+    const formData = new FormData();
+    formData.append("file", imageData);
+    formData.append("upload_preset", "qm48of0h");
+
+    axios
+      .post("https://api.cloudinary.com/v1_1/dbkmzgcap/image/upload", formData)
+      .then(({ data }) => {
+        return data.secure_url;
+      })
+      .then((data) => {
+        axios
+          .post("http://localhost:5000/a/test", {
+            title: title,
+            description: description,
+            image: data,
+          })
+          .then(({ data }) => console.log(data));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setImageData(file);
+      const reader = new FileReader();
+      setImageData;
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          setImage(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="w-full h-[90vh] flex items-center justify-center">
+      <UploadForm
+        onSubmit={onSubmit}
+        changeTitle={setTitle}
+        changeDescription={setDescription}
+        uploadImage={handleImageChange}
+      />
+      <div className="w-[1300px] h-[700px] flex items-center justify-center mx-auto rounded-xl bg-gray-800/10 p-2">
+        {image && (
+          <img className="max-w-full max-h-full" src={image} alt="Uploaded" />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
